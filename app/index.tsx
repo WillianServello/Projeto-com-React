3c1a32b149836afdff027ca1136eb6bb18afce42
WillianServello <rafaelwillian299@gmail.com>
2026-02-20 22:24:09 -0300

    ProjetandoTela

--- app/index.tsx
+++ app/index.tsx
@@ -2,4 +2,4 @@ import Page from "@/pages";
 
 export default function Index() {
   return <Page/>
-  }
+ }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 // <div/> = <view/>
 //<p> = <text/>
+
 import React from "react";
+import { GoGear } from "react-icons/go";
+
+import { FiShare2 } from "react-icons/fi";
+
 import { StyleSheet, Text, View } from "react-native";
+
 export default function Page(){
     // CODIGO JAVASCRIPT
     // CONSTANTES, FUNÇÕES, ETC..
@@ -12,17 +18,65 @@ export default function Page(){
 
     // PÁGINA QUE SERÁ RENDERIZADA
     return (
-        <View>
-            <Text>{nome}</Text>
-            <Text>WillianServello</Text>
+        <View style={[styles.background, styles.body]}>
+            <View>
+                <Text style={[styles.iconeColor, styles.iconePosition]}>
+                    <FiShare2 />
+                    <GoGear />
+                </Text>
+            </View>
+
+            <View style={styles.textMargin}>
+                <Text style={styles.text}>Willian Rafael Servello da Silva</Text>
+                <Text style={styles.textNickName}>WillianServello ° he/him</Text>
+            </View>
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    Background: {
+    background: {
         width: "100%",
         height: "100%",
-        backgroundColor: "blue"
+        backgroundColor: "black"
+    },
+
+    textMargin: {
+        marginTop: 40,
+        marginLeft: 70,
+        marginRight: 45,
+        
+    },
+
+    text: {
+        color: "white",
+        fontWeight: "bold",
+        fontFamily: "Arial",
+        fontSize: 24
+    },
+
+    textNickName:{
+        color: "#5B5B58",
+        marginTop: 5,
+        fontSize: 14
+    },
+
+    iconeColor:{
+        fontSize: 24,
+        color: "#5893ff"
+    },
+
+    iconePosition: {
+        marginLeft: "80%",
+        gap: 20,
+        display: "flex"
+    },
+
+    body:{
+        padding: 20
     }
+
+
+    
 })
