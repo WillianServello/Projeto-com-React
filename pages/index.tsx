724502b2296b7d959a9c448c9143860fe13d7f36
WillianServello <rafaelwillian299@gmail.com>
2026-02-23 17:01:47 -0300

    TerminandoTrabalho-React

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,25 @@
 //<p> = <text/>
 
 import React from "react";
-import { GoGear } from "react-icons/go";
 
-import { FiShare2 } from "react-icons/fi";
+import Octicons from '@expo/vector-icons/Octicons';
 
-import { FiSmile } from "react-icons/fi";
+import Feather from '@expo/vector-icons/Feather';
+
+import Entypo from '@expo/vector-icons/Entypo';
+
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+
+
+
+// import { FiShare2 } from "react-icons/fi";
+
+// import { FiSmile } from "react-icons/fi";
 
 import { StyleSheet, Text, View } from "react-native";
 
@@ -21,11 +35,11 @@ export default function Page(){
     // PÁGINA QUE SERÁ RENDERIZADA
     return (
         <View style={[styles.background, styles.body]}>
-            <View>
-                <Text style={[styles.iconeColor, styles.iconePosition]}>
-                    <FiShare2 />
-                    <GoGear />
-                </Text>
+            <View style={styles.iconesTopPosition}>
+
+                        <Feather style={{ marginRight: 10 }} name="share-2" size={24} color="#5893ff" />
+                        <Octicons name="gear" size={24} color="#5893ff"  />           
+                        
             </View>
 
             <View style={styles.textMargin}>
@@ -33,24 +47,94 @@ export default function Page(){
                 <Text style={styles.textNickName}>WillianServello ° he/him</Text>
             </View>
 
-            <View style={styles.caixaInput}>
-             
-                   
-                       
-                            <Text style={[styles.emojiColor, styles.configConfig]}>
-                                <FiSmile />
-                                Digite seu status
-                            </Text>
-                        
-                        
-                    
+            <View style={styles.caixaInput}>    
+
+                <View>
+                    <Entypo style={{marginRight: 10}} name="emoji-happy" size={16} color="#a6a6a6" />
+                </View>
+                
+                <Text style={{color: "#a6a6a6", fontSize: 16}}>      
+                     Definir seu status
+                </Text>
+                
+                <View style={{marginLeft: "auto", marginRight: 10}}>
+                    <Octicons name="pencil" size={16} color="#a6a6a6" />
+                </View>
+
+            </View>   
+
+            <View style={styles.sobreMimView}>
+                <Text style={styles.sobreMimText}>Sabor Desenvolvedor FullStack | C# | MYSQL | GitHub | Design  </Text>
             </View>
-            
+
+            <View style={styles.contatoView}>
+
+                <View style={styles.contatoViewAgrupamento}>
+                    <View style={styles.iconeContainer}>
+                        <MaterialIcons name="mail-outline" size={20} color="#a6a6a6" />
+                    </View>
+
+                    <Text style={styles.contatoText}>willianservello@gmail.com</Text>
+                </View>
+
+                <View style={styles.contatoViewAgrupamento}>
+                    <View style={styles.iconeContainer}>
+                        <AntDesign name="github" size={20} color="#a6a6a6" />
+                    </View>
+                    
+                    <Text style={styles.contatoText}>WillianServello</Text>
+                </View>
+
+                <View style={styles.contatoViewAgrupamento}>
+                    <View style={styles.iconeContainer}>
+                        <FontAwesome name="linkedin-square" size={20} color="#a6a6a6" />
+                        
+                    </View>
+                    
+                    <Text style={styles.contatoText}>in/willianservello</Text>
+                </View>
+
+                <View style={styles.contatoViewAgrupamento}>
+                    <View style={styles.iconeContainer}>
+                        <FontAwesome name="instagram" size={20} color="#a6a6a6" />
+                    </View>
+                    
+                    <Text style={styles.contatoText}>willianservello</Text>
+                </View>
+
+                <View style={styles.contatoViewAgrupamento}>
+                    <View style={styles.iconeContainer}>
+                        <FontAwesome name="steam-square" size={20} color="#a6a6a6" />  
+                    </View>
+                    
+                    <Text style={styles.contatoText}>relaxaplayer</Text>
+                </View>
+
+                <View style={styles.contatoViewAgrupamento}>
+                    <View style={styles.iconeContainer}>
+                        
+                        <FontAwesome name="user-o" size={16} color="#a6a6a6" />  
+                    </View>
+                    
+                    <Text style={styles.contatoText}>15 seguidores  °  15 seguindo</Text>
+
+                    
+                    
+                </View>
+                
+            </View>
+
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    
+    body:{
+        padding: 20
+    },
+
     background: {
         width: "100%",
         height: "100%",
@@ -62,6 +146,13 @@ const styles = StyleSheet.create({
         marginLeft: 70,
         marginRight: 45,
         
+    },
+
+    iconesTopPosition: {
+        flexDirection: "row",
+        justifyContent: "flex-end", 
+        marginTop: 30,
+        gap: 15, 
     },
 
     text: {
@@ -77,46 +168,56 @@ const styles = StyleSheet.create({
         fontSize: 14
     },
 
-    iconeColor:{
-        fontSize: 24,
-        color: "#5893ff"
-    },
-
-    iconePosition: {
-        marginLeft: "80%",
-        gap: 20,
-        display: "flex"
-    },
-
-    body:{
-        padding: 20
-    },
-
     caixaInput: {
-        padding: 10,
-        justifyContent: "center",
+        
+        flexDirection: "row", 
+        alignItems: "center", 
+        marginTop: 20,
+        paddingLeft: 12,
         borderRadius: 5,
-        marginTop: 15,
         backgroundColor: "#333333",
         width: "100%",
-        height: 38
-    },
-  
-    emojiColor: {
-        fontSize: 16,
-        color: "#a6a6a6",
-        gap: 5
-
+        height: "6%"
     },
 
-    alignText:{
+    sobreMimView: {
+        marginTop: 20,
+    },
+
+    sobreMimText: {
+        color: "white",
+        fontSize: 14,
+        fontFamily: "Arial",
+        lineHeight: 20
         
-        marginLeft: 10,
     },
 
-    configConfig: {
-        display: "flex",
+    contatoView: {
+        marginTop: 10,
+    },
+
+    contatoViewAgrupamento: {
+        marginTop: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10
+    },
+    
+    contatoText: {
+        color: "white",
+        fontSize: 14,
+        fontFamily: "Arial",
+        lineHeight: 20
+    },
+    
+  
+    iconeContainer: {
+        width: 20,          
         alignItems: "center"
-    }
+    },
+
+   
+
+   
     
 })
